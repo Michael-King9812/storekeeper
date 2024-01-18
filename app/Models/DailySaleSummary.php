bbdb3e255a8b45ecb04b8749e6ce882621e371a5
Michael-King9812 <michaelaranmonise@gmail.com>
2024-01-18 01:01:36 +0100

    Store Keeper commit

--- app/Models/DailySaleSummary.php
+++ app/Models/DailySaleSummary.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class DailySaleSummary extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'total_amount_due',
+        'total_amount_paid',
+        'cash',
+        'credit',
+        'pos',
+        'shortage_amount'
+    ];
 }
--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Sale extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'item_id',
+        'no_sold',
+        'total_amount',
+        'user_id'
+    ];
 }
